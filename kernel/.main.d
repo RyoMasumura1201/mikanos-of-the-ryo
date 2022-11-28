09b4daaad1714129686ea04b565bf1e95a110756
RyoMasumura1201 <ryomasumura1201@gmail.com>
2022-11-29 08:16:36 +0900

    day05b

---
+++ kernel/.main.d
@@ -0,0 +1,7 @@
+main.o: main.cpp \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  frame_buffer_config.hpp graphics.hpp font.hpp
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,84 +1,17 @@
+/**
+ * @file main.cpp
+ *
+ * カーネル本体のプログラムを書いたファイル．
+ */
+
 #include <cstdint>
 #include <cstddef>
 
+// #@@range_begin(includes)
 #include "frame_buffer_config.hpp"
-
-const uint8_t kFontA[16] = {
-  0b00000000, //
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b01111110, //  ******
-  0b01000010, //  *    *
-  0b01000010, //  *    *
-  0b01000010, //  *    *
-  0b11100111, // ***  ***
-  0b00000000, //
-  0b00000000, //
-};
-
-struct PixelColor {
-  uint8_t r, g, b;
-};
-
-class PixelWriter {
-  public:
-    PixelWriter(const FrameBufferConfig& config) : config_{config} {
-    }
-    virtual ~PixelWriter() = default;
-    virtual void Write(int x, int y, const PixelColor& c) = 0;
-  
-  protected:
-    uint8_t* PixelAt(int x, int y) {
-      return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
-    }
-  
-  private:
-    const FrameBufferConfig& config_;
-};
-
-class RGBResv8BitPerColorPixelWriter : public PixelWriter {
-  public:
-    using PixelWriter::PixelWriter;
-
-    virtual void Write(int x, int y, const PixelColor& c) override {
-      auto p = PixelAt(x, y);
-      p[0] = c.r;
-      p[1] = c.g;
-      p[2] = c.b;
-    }
-};
-
-class BGRResv8BitPerColorPixelWriter : public PixelWriter {
-  public:
-    using PixelWriter::PixelWriter;
-
-    virtual void Write(int x, int y, const PixelColor& c) override {
-      auto p = PixelAt(x, y);
-      p[0] = c.b;
-      p[1] = c.g;
-      p[2] = c.r;
-    }
-};
-
-void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color) {
-  if (c != 'A') {
-    return;
-  }
-
-  for (int dy = 0; dy < 16; ++dy) {
-    for (int dx = 0; dx < 8; ++dx) {
-      if ((kFontA[dy] << dx) & 0x80u) {
-        writer.Write(x + dx, y + dy, color);
-      }
-    }
-  }
-}
+#include "graphics.hpp"
+#include "font.hpp"
+// #@@range_end(includes)
 
 void* operator new(size_t size, void* buf) {
   return buf;
@@ -101,6 +34,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
         BGRResv8BitPerColorPixelWriter{frame_buffer_config};
       break;
   }
+
   for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
     for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
       pixel_writer->Write(x, y, {255, 255, 255});

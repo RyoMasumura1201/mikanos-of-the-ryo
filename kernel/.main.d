e29aab7a6ad6febfbac0d23aae07a625497ee393
RyoMasumura1201 <ryomasumura1201@gmail.com>
2022-11-30 08:33:16 +0900

    day05d

--- kernel/.main.d
+++ kernel/.main.d
@@ -4,4 +4,6 @@ main.o: main.cpp \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
   frame_buffer_config.hpp graphics.hpp font.hpp

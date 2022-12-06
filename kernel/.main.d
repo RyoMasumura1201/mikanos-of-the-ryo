d887fe629f68f80bb89bccf3f45d6e7912e1c931
RyoMasumura1201 <ryomasumura1201@gmail.com>
2022-12-06 09:03:56 +0900

    day06b

--- kernel/.main.d
+++ kernel/.main.d
@@ -6,4 +6,32 @@ main.o: main.cpp \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/version \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
-  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp
+  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp pci.hpp \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/array \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__tuple \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/type_traits \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/utility \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/initializer_list \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/string.h \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__debug \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/iterator \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/iosfwd \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/wchar.h \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__functional_base \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/typeinfo \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/exception \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstdlib \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/stdlib.h \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/new \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/algorithm \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/memory \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/limits \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/tuple \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/stdexcept \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cassert \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/assert.h \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/_ansi.h \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/functional \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/bit \
+  error.hpp

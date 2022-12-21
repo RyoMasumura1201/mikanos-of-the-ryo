2d2e952bc6be722f8ba75d272d3b5c76387fca9a
RyoMasumura1201 <ryomasumura1201@gmail.com>
2022-12-22 08:26:31 +0900

    day11e

--- kernel/.main.d
+++ kernel/.main.d
@@ -54,4 +54,5 @@ main.o: main.cpp \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__tree \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle \
   timer.hpp \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/queue
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/queue \
+  acpi.hpp

ed1ff7b7b283266f03b25dfa534bc39cbd89ad87
RyoMasumura1201 <ryomasumura1201@gmail.com>
2023-01-09 10:09:40 +0900

    day13b

--- kernel/.main.d
+++ kernel/.main.d
@@ -6,26 +6,27 @@ main.o: main.cpp \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/version \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/array \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__tuple \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/deque \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__split_buffer \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/type_traits \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/utility \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/algorithm \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/initializer_list \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/string.h \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/utility \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__tuple \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__debug \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/iterator \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/iosfwd \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/wchar.h \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__functional_base \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/memory \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/typeinfo \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/exception \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cstdlib \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/stdlib.h \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/new \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/algorithm \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/memory \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/limits \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/iterator \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/iosfwd \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/wchar.h \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__functional_base \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/tuple \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/stdexcept \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/cassert \
@@ -33,8 +34,6 @@ main.o: main.cpp \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/_ansi.h \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/functional \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/bit \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/deque \
-  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__split_buffer \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/numeric \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/vector \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__bit_reference \
@@ -43,16 +42,17 @@ main.o: main.cpp \
   frame_buffer_config.hpp memory_map.hpp graphics.hpp mouse.hpp font.hpp \
   console.hpp window.hpp \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/optional \
-  frame_buffer.hpp error.hpp pci.hpp logger.hpp usb/xhci/xhci.hpp \
-  usb/xhci/registers.hpp register.hpp usb/xhci/context.hpp \
-  usb/endpoint.hpp usb/xhci/ring.hpp usb/memory.hpp usb/xhci/trb.hpp \
-  usb/xhci/port.hpp usb/xhci/devmgr.hpp usb/xhci/device.hpp \
-  usb/device.hpp usb/setupdata.hpp usb/arraymap.hpp interrupt.hpp \
-  x86_descriptor.hpp message.hpp asmfunc.h segment.hpp paging.hpp \
-  memory_manager.hpp layer.hpp \
+  frame_buffer.hpp error.hpp \
+  /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/array \
+  pci.hpp logger.hpp usb/xhci/xhci.hpp usb/xhci/registers.hpp \
+  register.hpp usb/xhci/context.hpp usb/endpoint.hpp usb/xhci/ring.hpp \
+  usb/memory.hpp usb/xhci/trb.hpp usb/xhci/port.hpp usb/xhci/devmgr.hpp \
+  usb/xhci/device.hpp usb/device.hpp usb/setupdata.hpp usb/arraymap.hpp \
+  interrupt.hpp x86_descriptor.hpp message.hpp asmfunc.h segment.hpp \
+  paging.hpp memory_manager.hpp layer.hpp \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/map \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__tree \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle \
   timer.hpp \
   /home/ryo_masumura/osbook/devenv/x86_64-elf/include/c++/v1/queue \
-  acpi.hpp keyboard.hpp
+  acpi.hpp keyboard.hpp task.hpp
--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -2,6 +2,7 @@
 
 #include "acpi.hpp"
 #include "interrupt.hpp"
+#include "task.hpp"
 
 namespace {
   const uint32_t kCountMax = 0xffffffffu;
@@ -15,7 +16,7 @@ void InitializeLAPICTimer(std::deque<Message>& msg_queue) {
   timer_manager = new TimerManager{msg_queue};
 
   divide_config = 0b1011;
-  lvt_timer = (0b001 << 16);
+  lvt_timer = 0b001 << 16;
 
   StartLAPICTimer();
   acpi::WaitMilliseconds(100);
@@ -54,12 +55,21 @@ void TimerManager::AddTimer(const Timer& timer) {
   timers_.push(timer);
 }
 
-void TimerManager::Tick() {
+bool TimerManager::Tick() {
   ++tick_;
+
+  bool task_timer_timeout = false;
   while (true) {
     const auto& t = timers_.top();
     if (t.Timeout() > tick_) {
       break;
+    }
+
+    if (t.Value() == kTaskTimerValue) {
+      task_timer_timeout = true;
+      timers_.pop();
+      timers_.push(Timer{tick_ + kTaskTimerPeriod, kTaskTimerValue});
+      continue;
     }
 
     Message m{Message::kTimerTimeout};
@@ -69,11 +79,18 @@ void TimerManager::Tick() {
 
     timers_.pop();
   }
+
+  return task_timer_timeout;
 }
 
 TimerManager* timer_manager;
 unsigned long lapic_timer_freq;
 
 void LAPICTimerOnInterrupt() {
-  timer_manager->Tick();
+  const bool task_timer_timeout = timer_manager->Tick();
+  NotifyEndOfInterrupt();
+
+  if (task_timer_timeout) {
+    SwitchTask();
+  }
 }
